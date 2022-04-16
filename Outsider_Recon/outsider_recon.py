a5b9165b47c7ed56305eb9059cc8088052585175
Cody Martin <cody.martin@blacklanternsecurity.com>
2022-04-15 22:19:15 -0400

    cleaning outsider_recon

--- Outsider_Recon/outsider_recon.py
+++ Outsider_Recon/outsider_recon.py
@@ -17,11 +17,42 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-import os, requests, sys, argparse, colorama, time, json, dns.resolver, dns.rcode
+import sys
+import time
+import json
+import dns.resolver
+import dns.rcode
 import xml.etree.ElementTree as ET
+import argparse
+import colorama
+import requests
 
-class Outsider_Recon:
+class OutsiderRecon:
+	'''
+	Contains all functions necessary to enumerate an Azure tenant
+	given a domain that belongs to an Azure tenant.
 
+	Methods
+	-------
+	enumerate_domain_info(domains, login_infos):
+		Enumerates information about a domain, including DMARC, CloudSPF, CloudMX, DNS, STS, SSO
+
+	enumerate_tenant_id(openid_config):
+		Given an openid_config, will return the tenant ID
+
+	enumerate_login_info(domain, username):
+		Given a domain and optional username, will return the authentication related endpoints
+		and information as they pertain to the supplied domain
+
+	enumerate_openid(domain):
+		Given a domain, will return the openid configuration information
+
+	enumerate_tenant_domains(domain, user_agent='AutodiscoverClient'):
+		Given a domain and optional user_agent, will return all domains
+		registered to the same Azure tenant as the domain provided
+	'''
+
+	@staticmethod
 	def enumerate_domain_info(self, domains, login_infos):
 		domain_info = {}
 		for domain in domains:
@@ -32,19 +63,19 @@ class Outsider_Recon:
 			try:
 				if login_infos[domain]['Desktop SSO Enabled'] == 'True':
 					domain_info[domain]['sso'] = True
-			except:
-				None
+			except KeyError as key_error:
+				pass
 
 			# Check for Namespace
 			try:
 				domain_info[domain]['type'] = login_infos[domain]['Namespace Type']
-			except:
+			except KeyError as key_error:
 				domain_info[domain]['type'] = 'Unknown'
 
 			# Check for STS
 			try:
 				domain_info[domain]['sts'] = login_infos[domain]['Authentication URL']
-			except:
+			except KeyError as key_error:
 				domain_info[domain]['sts'] = ''
 
 			# Check if DNS Name resolves
@@ -58,7 +89,7 @@ class Outsider_Recon:
 					domain_info[domain]['cloudspf'] = False
 					domain_info[domain]['dmarc'] = False
 					continue
-			except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer) as na:
+			except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer) as no_answer:
 				domain_info[domain]['dns'] = False
 				domain_info[domain]['cloudmx'] = False
 				domain_info[domain]['cloudspf'] = False
@@ -69,46 +100,47 @@ class Outsider_Recon:
 			try:
 				domain_info[domain]['cloudmx'] = False
 				dns_response = dns.resolver.resolve(domain, 'MX')
-				for a in dns_response:
-					if 'mail.protection.outlook.com' in str(a):
+				for answer in dns_response:
+					if 'mail.protection.outlook.com' in str(answer):
 						domain_info[domain]['cloudmx'] = True
 						break
-			except Exception as e:
-				None
+			except dns.exception.DNSException as dns_exception:
+				pass
 			# Check for CloudSPF
 			try:
 				domain_info[domain]['cloudspf'] = False
 				dns_response = dns.resolver.resolve(domain, 'TXT')
-				for a in dns_response:
-					if 'include:spf.protection.outlook.com' in str(a):
+				for answer in dns_response:
+					if 'include:spf.protection.outlook.com' in str(answer):
 						domain_info[domain]['cloudspf'] = True
 						break
-			except Exception as e:
-				None
+			except dns.exception.DNSException as dns_exception:
+				pass
 			# Check for DMARC
 			try:
 				domain_info[domain]['dmarc'] = False
 				dns_response = dns.resolver.resolve('_dmarc.' + domain, 'TXT')
-				for a in dns_response:
-					if 'v=DMARC1' in str(a):
+				for answer in dns_response:
+					if 'v=DMARC1' in str(answe)r:
 						domain_info[domain]['dmarc'] = True
 						break
-			except Exception as e:
-				None
-				
+			except dns.exception.DNSException as dns_exception:
+				pass
+
 		return domain_info
 
+	@staticmethod
 	def enumerate_tenant_id(self, openid_config):
 
 		return openid_config['authorization_endpoint'].split('/')[3]
 
-
+	@staticmethod
 	def enumerate_login_info(self, domain, username):
-		
+
 		results = {}
 
 		user = username + '@' + domain
-		
+
 		endpoint1 = 'https://login.microsoftonline.com/common/userrealm/%s?api-version=1.0' % (user)
 		endpoint2 = 'https://login.microsoftonline.com/common/userrealm/%s?api-version=2.0' % (user)
 		endpoint3 = 'https://login.microsoftonline.com/GetUserRealm.srf?login=%s' % (user)
@@ -135,90 +167,91 @@ class Outsider_Recon:
 		user_realm_json3 = requests.get(endpoint3).json()
 		user_realm_json4 = requests.post(endpoint4, headers=headers4, data=json_data).json()
 
-		
+
 		try:
 			results['Account Type'] = user_realm_json1['account_type']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Namespace Type'] = user_realm_json2['NameSpaceType']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Domain Name'] = user_realm_json3['DomainName']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Cloud Instance'] = user_realm_json1['cloud_instance_name']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Cloud Instance Audience URN'] = user_realm_json1['cloud_audience_urn']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Federation Brand Name'] = user_realm_json3['FederationBrandName']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['State'] = user_realm_json3['State']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['User State'] = user_realm_json3['UserState']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Exists'] = user_realm_json4['IfExistsResult']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Throttle Status'] = user_realm_json4['ThrottleStatus']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Pref Credential'] = user_realm_json4['Credentials']['PrefCredential']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Has Password'] = user_realm_json4['Credentials']['HasPassword']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Domain Type'] = user_realm_json4['EstsProperties']['DomainType']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Federation Protocol'] = user_realm_json1['federation_protocol']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Federation Metadata URL'] = user_realm_json1['federation_metadata_url']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Federation Active Authentication URL'] = user_realm_json1['federation_active_auth_url']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Authentication URL'] = user_realm_json2['AuthUrl']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Consumer Domain'] = user_realm_json2['ConsumerDomain']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Federation Global Version'] = user_realm_json3['FederationGlobalVersion']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 		try:
 			results['Desktop SSO Enabled'] = user_realm_json4['EstsProperties']['DesktopSsoEnabled']
-		except KeyError as ke:
-			None
+		except KeyError as key_error:
+			pass
 
 		return results
 
+	@staticmethod
 	def enumerate_openid(self, domain):
 
 		endpoint = 'https://login.microsoftonline.com/%s/.well-known/openid-configuration' % (domain)
@@ -227,7 +260,7 @@ class Outsider_Recon:
 
 		return openid_config_json
 
-
+	@staticmethod
 	def enumerate_tenant_domains(self, domain, user_agent='AutodiscoverClient'):
 		headers = {
 			'Content-Type': 'text/xml; charset=utf-8',
@@ -254,12 +287,12 @@ class Outsider_Recon:
 		</soap:Envelope>''' % (domain)
 
 		endpoint = 'https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc'
-		
+
 		# Get Tenant Domains with Supplied Domain
 		# Returns a SOAP Envelope object
 		# Loops until we receive valid data
 		proceed = False
-		while(proceed == False):
+		while not proceed:
 			tenant_domains = requests.post(endpoint, data=xml, headers=headers)
 			if tenant_domains.status_code == 421:
 				return None
@@ -277,14 +310,14 @@ class Outsider_Recon:
 
 		return domains
 
-
+	@staticmethod
 	def main(self):
 		# Set up our colors
 		colorama.init()
-		success = colorama.Fore.GREEN
-		danger = colorama.Fore.RED
-		warning = colorama.Fore.YELLOW
-		reset = colorama.Style.RESET_ALL
+		SUCCESS = colorama.Fore.GREEN
+		DANGER = colorama.Fore.RED
+		WARNING = colorama.Fore.YELLOW
+		RESET = colorama.Style.RESET_ALL
 
 		description = '''
   =====================================================================================
@@ -293,29 +326,35 @@ class Outsider_Recon:
   =====================================================================================
 '''
 
-		arg_parser = argparse.ArgumentParser(prog='outsider-recon.py', 
-											 usage=success + '%(prog)s' + warning + ' <domain>' + reset + ' [-o|--outfile <path-to-file>] [-u|--user <user>]',
-											 description=description,
-											 formatter_class=argparse.RawDescriptionHelpFormatter
-											 )
-		arg_parser.add_argument('Domain',
-								metavar='domain',
-								type=str,
-								help='The target Microsoft/Azure domain')
-		arg_parser.add_argument('-o',
-								'--outfile-path',
-								metavar='<path>',
-								dest='outfile_path',
-								type=str,
-								help='(string) The path where you want the recon data (json object) saved.\nIf not supplied, module defaults to the current directory',
-								required=False)
-		arg_parser.add_argument('-u',
-								'--user',
-								metavar='<username>',
-								dest='user',
-								type=str,
-								help='(string) The user you want to use during enumeration. Do not include the domain.\nIf not supplied, module defaults to "none"',
-								required=False)
+		arg_parser = argparse.ArgumentParser(
+			prog='outsider_recon.py',
+			usage=SUCCESS + '%(prog)s' + WARNING + ' <domain>' + RESET + \
+				' [-o|--outfile <path-to-file>] [-u|--user <user>]',
+			description=description,
+			formatter_class=argparse.RawDescriptionHelpFormatter)
+		arg_parser.add_argument(
+			'Domain',
+			metavar='domain',
+			type=str,
+			help='The target Microsoft/Azure domain')
+		arg_parser.add_argument(
+			'-o',
+			'--outfile-path',
+			metavar='<path>',
+			dest='outfile_path',
+			type=str,
+			help='(string) The path where you want the recon data (json object) saved.\n' \
+				'If not supplied, module defaults to the current directory',
+			required=False)
+		arg_parser.add_argument(
+			'-u',
+			'--user',
+			metavar='<username>',
+			dest='user',
+			type=str,
+			help='(string) The user you want to use during enumeration. Do not include the' \
+				' domain.\nIf not supplied, module defaults to "none"',
+			required=False)
 
 		args = arg_parser.parse_args()
 
@@ -323,92 +362,92 @@ class Outsider_Recon:
 
 		# Set a default path if none is given
 		path = args.outfile_path
-		if path == None:
+		if path is None:
 			path = './'
 		elif path[-1] != '/':
 			path = path + '/'
 
 		# Set a default user if none is given
 		user = args.user
-		if user == None:
+		if user is None:
 			user = 'none'
-		
+
 		# Enumerating all domains for the tenant the passed in domain belongs to
-		print(warning + 'Enumerating Other Domains Within Tenant' + reset + '\n')
+		print(WARNING + 'Enumerating Other Domains Within Tenant' + RESET + '\n')
 		domains_found = self.enumerate_tenant_domains(args.Domain)
-		if domains_found == None:
-			print(danger + 'It doesn\'t look like this is a domain in Azure. Check your domain or try something else.')
+		if domains_found is None:
+			print(DANGER + 'It doesn\'t look like this is a domain in Azure. Check your domain or try something else.')
 			sys.exit()
 		for domain_found in domains_found:
-			print(success + '[+] ' + reset + domain_found)
+			print(SUCCESS + '[+] ' + RESET + domain_found)
 		print()
-		
+
 		# Enumerating the openid configuration for the tenant
-		print(warning + 'Enumerating OpenID Configuration for Tenant' + reset + '\n')
+		print(WARNING + 'Enumerating OpenID Configuration for Tenant' + RESET + '\n')
 		openid_config = self.enumerate_openid(args.Domain)
 		for elem in openid_config:
-			print((success + elem + reset + ':\t' + str(openid_config[elem])).expandtabs(50))
+			print((SUCCESS + elem + RESET + ':\t' + str(openid_config[elem])).expandtabs(50))
 		print()
-		
+
 		# Enumerating the login information for each domain discovered
 		login_infos = {}
-		print(warning + 'Enumerating User Login Information' + reset + '\n')
+		print(WARNING + 'Enumerating User Login Information' + RESET + '\n')
 		for domain_found in domains_found:
 			user_realm_json = self.enumerate_login_info(args.Domain, user)
 			login_infos[domain_found] = user_realm_json
-			print(warning + '[+] ' + domain_found + ":" + reset)
-			print(warning + '========================' + reset)
+			print(WARNING + '[+] ' + domain_found + ":" + RESET)
+			print(WARNING + '========================' + RESET)
 			for elem in user_realm_json:
-				print((success + elem + reset + ":\t" + str(user_realm_json[elem])).expandtabs(50))
-			print(warning + '========================' + reset + '\n')
+				print((SUCCESS + elem + RESET + ":\t" + str(user_realm_json[elem])).expandtabs(50))
+			print(WARNING + '========================' + RESET + '\n')
 		print()
 
 		# Enumerate the tenant ID
-		print(warning + 'Tenant ID' + reset + '\n')
+		print(WARNING + 'Tenant ID' + RESET + '\n')
 		tenant_id = self.enumerate_tenant_id(openid_config)
-		print(success + '[+] ' + reset + tenant_id)
+		print(SUCCESS + '[+] ' + RESET + tenant_id)
 		print()
 
 		# Enumerate Domain Information (DNS, CLOUDMX, CLOUDSPF, DMARC, Identity Management, STS, SSO)
-		print(warning + 'Enumerating Domain Information' + reset + '\n')
+		print(WARNING + 'Enumerating Domain Information' + RESET + '\n')
 		domain_info = self.enumerate_domain_info(domains_found, login_infos)
 		for domain_name in domain_info:
-			print(warning + '[+] ' + domain_name + ":" + reset)
-			print(warning + '========================' + reset)
+			print(WARNING + '[+] ' + domain_name + ":" + RESET)
+			print(WARNING + '========================' + RESET)
 			for key, value in domain_info[domain_name].items():
-				print((success + key + reset + ":\t" + str(value)).expandtabs(24))
-			print(warning + '========================' + reset + '\n')
+				print((SUCCESS + key + RESET + ":\t" + str(value)).expandtabs(24))
+			print(WARNING + '========================' + RESET + '\n')
 
 		# Save our results to files
-		
+
 		## Save Domain List
-		f = open(path + outfile_prefix + 'domain_list.txt', 'w+')
-		for dom in domains_found:
-			f.write(dom + '\n')
-		f.close()
+		with open(path + outfile_prefix + 'domain_list.txt', 'w+', encoding='UTF-8') as file:
+			for dom in domains_found:
+				file.write(dom + '\n')
+			file.close()
 
 		## Save Tenant OpenID Configuration
-		f = open(path + outfile_prefix + 'tenant_openid_config.json', 'w+')
-		f.write(json.dumps(openid_config))
-		f.close()
+		with open(path + outfile_prefix + 'tenant_openid_config.json', 'w+', encoding='UTF-8') as file:
+			file.write(json.dumps(openid_config))
+			file.close()
 
 		## Save Domain Login Information
-		f = open(path + outfile_prefix + 'domain_login_information.json', 'w+')
-		f.write(json.dumps(login_infos))
-		f.close()
+		with open(path + outfile_prefix + 'domain_login_information.json', 'w+', encoding='UTF-8') as file:
+			file.write(json.dumps(login_infos))
+			file.close()
 
 		## Save Tenant ID
-		f = open(path + outfile_prefix + 'tenant_id.txt', 'w+')
-		f.write(tenant_id)
-		f.close()
+		with open(path + outfile_prefix + 'tenant_id.txt', 'w+', encoding='UTF-8') as file:
+			file.write(tenant_id)
+			file.close()
 
 		## Save Domain Information
-		f = open(path + outfile_prefix + 'domain_information.json', 'w+')
-		f.write(json.dumps(domain_info))
-		f.close()
+		with open(path + outfile_prefix + 'domain_information.json', 'w+', encoding='UTF-8') as file:
+			file.write(json.dumps(domain_info))
+			file.close()
 
-		print(success + '[+] Files Saved Successfully!' + reset)
+		print(SUCCESS + '[+] Files Saved Successfully!' + RESET)
 
 if __name__ == '__main__':
-	prog = Outsider_Recon()
+	prog = OutsiderRecon()
 	prog.main()
